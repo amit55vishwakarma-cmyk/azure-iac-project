3c09763d2f4ddb2015c7663741bcf055637119a3
amit55vishwakarma-cmyk <amit55vishwakarma-cmyk>
2026-08-12 21:52:40 +0530

    Format Terraform configuration files

--- child_module/azurerm_virtual_network/main.tf
+++ child_module/azurerm_virtual_network/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet" {
-    for_each = var.vnet
+  for_each            = var.vnet
   name                = each.value.name
   resource_group_name = each.value.resource_group_name
   location            = each.value.location
--- parent_module/main.tf
+++ parent_module/main.tf
@@ -1,35 +1,35 @@
 module "rgs" {
   source = "../child_module/azurerm_resource_group"
-  rgs = var.rgs
+  rgs    = var.rgs
 }
 
 module "vnet" {
-  depends_on = [ module.rgs ]
-  source = "../child_module/azurerm_virtual_network"
-  vnet = var.vnet
+  depends_on = [module.rgs]
+  source     = "../child_module/azurerm_virtual_network"
+  vnet       = var.vnet
 }
 
 module "subnet1" {
-  depends_on = [ module.vnet ]
-  source = "../child_module/azurerm_subnet"
-  subnet1 =var.subnet1
+  depends_on = [module.vnet]
+  source     = "../child_module/azurerm_subnet"
+  subnet1    = var.subnet1
 }
 
 module "public_ip" {
-  depends_on = [ module.rgs ]
-  source = "../child_module/azurerm_public_ip"
-  public_ip = var.public_ip
-  
+  depends_on = [module.rgs]
+  source     = "../child_module/azurerm_public_ip"
+  public_ip  = var.public_ip
+
 }
 
 module "nic" {
-  depends_on = [ module.subnet1,module.public_ip ]
-  source = "../child_module/azurerm_network_interface_card"
-  nic = var.nic
+  depends_on = [module.subnet1, module.public_ip]
+  source     = "../child_module/azurerm_network_interface_card"
+  nic        = var.nic
 }
 
 module "vms" {
-  depends_on = [ module.nic ]
-  source = "../child_module/azurerm_virtual_machine"
-  vms = var.vms
+  depends_on = [module.nic]
+  source     = "../child_module/azurerm_virtual_machine"
+  vms        = var.vms
 }
