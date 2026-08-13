bef2ff7bc92ae65f33500adc63ef481fa3c1e543
amit55vishwakarma-cmyk <amit55vishwakarma-cmyk>
2026-08-13 23:09:22 +0530

    updated code

--- child_module/azurerm_network_interface_card/variable.tf
+++ child_module/azurerm_network_interface_card/variable.tf
@@ -1,10 +1,10 @@
 variable "nic" {
   type = map(object({
-    name                = string
-    location            = string
-    resource_group_name = string
+    name                 = string
+    location             = string
+    resource_group_name  = string
     subnet_name          = string
     virtual_network_name = string
-    pip_name             = string
+    pip_name              = string
   }))
 }
--- parent_module/variable.tf
+++ parent_module/variable.tf
@@ -34,11 +34,11 @@ variable "public_ip" {
 
 variable "nic" {
   type = map(object({
-    name                = string
-    location            = string
-    resource_group_name = string
+    name                 = string
+    location             = string
+    resource_group_name  = string
     subnet_name          = string
     virtual_network_name = string
-    pip_name             = string
+    pip_name              = string
   }))
 }
