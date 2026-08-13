43d0e002f50bef8f9570d7985b84ce7bd303a033
amit55vishwakarma-cmyk <amit55vishwakarma-cmyk>
2026-08-13 22:18:22 +0530

    code modified

--- child_module/azurerm_public_ip/variable.tf
+++ child_module/azurerm_public_ip/variable.tf
@@ -1,1 +1,8 @@
-variable "public_ip" {}
+variable "public_ip" {
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    location            = string
+    allocation_method   = string
+  }))
+}
