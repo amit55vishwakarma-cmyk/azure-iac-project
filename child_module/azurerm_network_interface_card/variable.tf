73d606415c3a3387320de3f36777eb2dd4efa128
amit55vishwakarma-cmyk <amit55vishwakarma-cmyk>
2026-08-13 23:05:42 +0530

    updated code

--- child_module/azurerm_network_interface_card/variable.tf
+++ child_module/azurerm_network_interface_card/variable.tf
@@ -3,5 +3,8 @@ variable "nic" {
     name                = string
     location            = string
     resource_group_name = string
+    subnet_name          = string
+    virtual_network_name = string
+    pip_name             = string
   }))
 }
--- parent_module/variable.tf
+++ parent_module/variable.tf
@@ -37,5 +37,8 @@ variable "nic" {
     name                = string
     location            = string
     resource_group_name = string
+    subnet_name          = string
+    virtual_network_name = string
+    pip_name             = string
   }))
 }
